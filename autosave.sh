5d8b2018460a3d7fa91b951bb875dad27e985b06
daneos <daneos@daneos.com>
2012-12-13 13:55:36 +0100

    error messages

--- autosave.sh
+++ autosave.sh
@@ -12,8 +12,8 @@ iptables-save | sed -e "/^#.*/ d" -e "/^:.*/ s/\[[0-9:]*\]//" > $newfile
 
 if [ -n "$(cmp $oldfile $newfile)" ]; then
 	iptables-save > $tmpfile
-	mv $savefile $savefile.bak
-	mv $tmpfile $savefile
+	cp $savefile $savefile.bak || { echo "Cannot make backup file."; exit 1; }
+	mv $tmpfile $savefile || { echo "Cannot move temp file to $savefile"; exit 1; }
 	diff $oldfile $newfile | mail -s "iptables autosave: $(cat /etc/hostname): saved new iptables rules" root
 fi
 
